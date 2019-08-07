89772a192c5d66b48f169ce9d12dbb127ebf8ae0
20160818 <devtinatina@gmail.com>
2019-08-07 15:34:52 +0900

    change color

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = 700;
 canvas.height = 700;
 
-ctx.strockeStyle = '#2c2c2c';
+ctx.strokeStyle = '#2c2c2c';
 ctx.lineWidth = 2.5;
 
 let painting = false;
 
-function stopPainting(){
+function stopPainting() {
     painting = false;
 }
 
@@ -20,7 +21,7 @@ function startPainting() {
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
-    if(!painting){
+    if (!painting) {
         ctx.beginPath();
         ctx.moveTo(x, y);
     } else {
@@ -29,13 +30,15 @@ function onMouseMove(event) {
     }
 }
 
-function onMouseDown(event) {
-    painting = true;
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
 }
-
-if(canvas){
+if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
-} 
+}
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
